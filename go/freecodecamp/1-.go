4db323344bc967ab04cedef8a7b7859e2a6de69c
stonkol <hector96fi@yahoo.es>
2024-02-21 22:22:09 +0100

    mini update susu

---
+++ go/freecodecamp/1-.go
@@ -0,0 +1,13 @@
+// Go, also known as Golang due to its domain name (golang.org),
+// is a statically typed, compiled programming language designed
+// at Google by Robert Griesemer, Rob Pike, and Ken Thompson. 
+// It was officially announced in November 2009 and has since 
+// gained popularity for its simplicity, efficiency, and strong 
+// support for concurrent programming.
+//
+// Is more simple to deploy a compiled server program ->
+//    Because there are no runtime language dependancies
+//
+// Static typed get errors more quicky
+//
+//
